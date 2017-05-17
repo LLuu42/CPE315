500b1b8ca4d08345c4b550189e3820117ed691d2
LLuu42 <ljluu@calpoly.edu>
2017-05-17 10:20:07 -0700

    lab 4 init commit

---
+++ lab4/Label.java
@@ -0,0 +1,28 @@
+/**
+* Class Label
+* @author Lara Luu
+* 26 April 2017
+* Stores the information for MIPS labels by name and address location 
+*/
+public class Label
+{
+	private String label;
+	private int address;
+
+	public Label(String lable, int address)
+	{
+		this.label = lable;
+		this.address = address;
+	}
+	
+	public String getLabel()
+	{
+		return this.label;
+	}
+	
+	public int getAddress()
+	{
+		return this.address;
+	}
+
+}
